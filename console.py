92f1df0b6dd3366343ec1ac70ab9383b1b1dbc9c
frankkib <frankkibagendi13@gmail.com>
2023-03-13 08:32:33 +0000

    fixed the pycode error

--- console.py
+++ console.py
@@ -6,10 +6,13 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """prompt class """
     prompt = "(hbnb)"
+
     def emptyline(self):
         pass
+
     def do_EOF(self, line):
         return True
+
     def do_quit(self, line):
         "Quit command to exit the program"
         return True
