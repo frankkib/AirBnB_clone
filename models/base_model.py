f4b83391748f8f0d97c317e3b09c96c21ac79b31
frankkib <frankkibagendi13@gmail.com>
2023-03-13 08:34:21 +0000

    fixed the pycode error

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,8 @@ class BaseModel:
 
     def __str__(self):
         """return a string"""
-        return "[{}]({}){}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}]({}){}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """upadates the atrributes"""
